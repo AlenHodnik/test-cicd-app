39d38c5334bf060fe7518d20571e1792f48de8f4
AlenHodnik <alen.hodnik@jsp.si>
2026-03-05 14:14:02 +0100

    Initial test CI/CD app

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.get("/")
+def hello():
+    return {
+        "ok": True,
+        "message": "Hello from CI/CD!",
+        "env": os.getenv("APP_ENV", "unknown")
+    }
+
+@app.get("/health")
+def health():
+    return "ok", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
